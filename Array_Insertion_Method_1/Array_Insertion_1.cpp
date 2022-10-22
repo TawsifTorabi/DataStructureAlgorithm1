bfafc1bee4eadcfa061df3de5cc3b6cd78630682
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-10-23 02:06:23 +0600

    Array Insertion

---
+++ Array_Insertion_Method_1/Array_Insertion_1.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //Insertion
+    int s;
+    cout << "Input Array Size:" << endl;
+    cin >> s;
+    int x[s+1];
+    int loc, item;
+
+    cout << "Input Array Elements:" << endl;
+    //Traversing
+    for(int i=0; i<s; i++){
+        cin >> x[i];
+    }
+
+    //Printing The Array
+    for(int i=0; i<s; i++){
+        cout << x[i] << " ";
+    }
+    cout << endl;
+
+
+
+    cout<<"Enter Value to Insert:" << endl;
+    cin>>item;
+
+
+    cout<<"Enter Location:" << endl;
+    cin>>loc;
+
+
+    for(int i=s-1; i>=loc; i--){
+        x[i+1] = x[i];
+    }
+    x[loc] = item;
+
+
+    //Printing The New Array
+    for(int i=0; i<s+1; i++){
+        cout << x[i] << " ";
+    }
+    cout << endl;
+
+
+    return 0;
+}
