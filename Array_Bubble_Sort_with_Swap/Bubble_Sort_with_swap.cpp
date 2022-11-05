f3d4493db6729b6ea1136566ea2341f0fae3bc24
Tawsif Torabi <tawsiftorabi.jr@gmail.com>
2022-11-05 15:02:17 +0600

    Update and rename Array_Bubble_Sort/Bubble_Sort.cpp to Array_Bubble_Sort_with_Swap/Bubble_Sort_with_swap.cpp

--- Array_Bubble_Sort_with_Swap/Bubble_Sort_with_swap.cpp
+++ Array_Bubble_Sort_with_Swap/Bubble_Sort_with_swap.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+void Swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 // A function to implement bubble sort
 void bubbleSort(int arr[], int n)
 {
     int i, j;
     for (i = 0; i<n-1; i++){
-        // Last i elements are already
-        // in place
+        // Last i elements are already in place
         for (j = 0; j<n-i-1; j++){
             if (arr[j] > arr[j + 1]){
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                Swap(&arr[j], &arr[j+1]);
                 
             }
         }
     }
 }
+
 
 int main(){
    //Input array size
